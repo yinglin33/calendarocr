af349b3ae6558974df3555ec2afc4c2b23c5219b
bananahbatman <fasterthanlight@gmail.com>
2020-11-29 20:02:45 +0000

    turned image_processing into a function

--- image_processing.py
+++ image_processing.py
@@ -1,8 +1,10 @@
 import cv2
 import pytesseract
+def process_image(filename)
 
-img = cv2.imread('tester.jpg')
+    img = cv2.imread(filename)
 
-# Adding custom options
-custom_config = '--oem 3 --psm 6'
-print(pytesseract.image_to_string(img, config=custom_config))
+    # Adding custom options
+    custom_config = '--oem 3 --psm 6'
+
+    return pytesseract.image_to_string(img, config=custom_config)
