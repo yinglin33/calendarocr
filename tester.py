c5be79cb828afc430d4d6561a88dbe355f9775d6
Ying Lin <yinglonglin@berkeley.edu>
2020-11-29 19:43:45 -0800

    added text from image files as variables

--- tester.py
+++ tester.py
@@ -1,3 +1,56 @@
-import image_processing
+#import image_processing
 
-process_image('image_processing tester.jpg') 
+TEXT_1 = """
+how to create
+random, sample text
+in microsoft word
+\u00a9 kent L\u00f6fgren, Sweden
+        """
+
+print(TEXT_1)
+
+TEXT_2 = """
+engineers
+on the green
+engineering organization fair
+monday, september 30, 2019
+2pm - 6pm \u2022 warren mall
+meet orgs, get free food, and have fun
+"""
+
+print(TEXT_2)
+
+TEXT_3 = """
+acm eats @ in-n-out
+10/03/19 @ 9:00pm. 2910 damon ave, san diego, ca 92109
+"""
+
+print(TEXT_3)
+
+#what to do about the C++ symbol?
+TEXT_4 = """
+acm x tse
+advance c++ workshops
+november 8 & 15 6:30-9 pm
+tse.ucsd.edu
+"""
+
+print(TEXT_4)
+
+TEXT_5 = """
+poems. 35
+xxi.
+a book.
+he ate and drank the precious words,
+his spirit grew robust;
+he knew no more that he was poor,
+nor that his frame was dust.
+he danced along the dingy days,
+and this bequest of wings
+was but a book. what liberty
+a loosened spirit brings!
+"""
+
+print(TEXT_5)
+
+#process_image('image_processing tester.jpg')
