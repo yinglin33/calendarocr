2f46e6f0d0d18c2da6bf5892cef6c7346cbccba3
bananahbatman <fasterthanlight@gmail.com>
2020-12-30 01:36:47 +0000

    Added method to test convertToCalendar

---
+++ calendartester.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import datetime
+import sys
+from image_processing import convertToCalendar
+import difflib #library for comparing differences
+
+def calendarTest():
+    testEvents = ["Engineers on the Green Engineering Organization Fair Monday, September 30, 2020 2pm - 6pm * Warren Mall","ACM EATS @ IN-N-OUT 10/03/20 @ 9:00pm 2910 Damon Ave, San Diego, CA 92109", "ACM x TSE Advanced C++ Workshops November 8 & 15 6:30-9 pm tsa.ucsd.edu", "" ]  
+    test = []
+    test_name = []
+    test1 = datetime.datetime(2020, 9, 30, 14, 0, 0 )
+    test2 = datetime.datetime(2020, 10, 3, 21, 0, 0 )
+    test3 = datetime.datetime(2020, 9, 30, 14, 0, 0 )
+    test4 = datetime.datetime(2020, 1, 1, 0, 0, 0 )
+    test.append(test1)
+    test.append(test2)
+    test.append(test3)
+    test.append(test4) 
+    test5 = datetime.datetime(2020, 9, 30, 14, 0, 0 ) 
+   
+    test_name.append("Engineers on the Green Engineering Organization Fair")
+    test_name.append("ACM EATS @ IN-N-OUT")
+    test_name.append("ACM x TSE Advanced C++ Workshops") 
+    test_name.append("")
+    
+
+    for i in range(len(testEvents)):
+        temp1, temp2 = convertToCalendar(testEvents[i])
+        print("Expected date: " + test[i].strftime("%m/%d/%Y, %H:%M")) 
+        print("Expected name: " + test_name[i])
+        print("calendarTest date: " + temp1.strftime("%m/%d/%Y, %H:%M")) 
+        print("calendarTest name: " + temp2)
+
+if __name__ == '__main__':
+    calendarTest()
+
--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,8 @@
-from datetime import date, time, datetime
+import datetime
 import cv2
 from pytesseract import image_to_string
+from PIL import Image
+
 
 def process_image(filename):
 
@@ -13,18 +15,18 @@ def process_image(filename):
     # Adding custom options
     custom_config = '--oem 3 --psm 6'
 
-    return pytesseract.image_to_string(img, config=custom_config)
+#    return pytesseract.image_to_string(img, config=custom_config)
 
-def convertToCalendar(cal)
+def convertToCalendar(cal):
     months_short=["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
     dates =[31,28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     months_long = ["january","february","march","april","may","june","july","august","september","october","november","december" ]
     #todo account for leap years for feburary
-    Calendar= datetime.datetime(2020, -1,-1, 0, 0, 0)
+    Calendar= datetime.datetime(2020, 1, 1, 0, 0, 0)
     # the parameters are year, month, day, hour, minutes, seconds
     index= -1
     #might be a good idea to remove spaces from string to help with processing
-    today = date.today()
+    today = datetime.datetime.today()
     cal=cal.lower()
     event_name = ""
 
@@ -33,9 +35,9 @@ def convertToCalendar(cal)
         cal_del_index = cal.find(months_short[i])
         cal_del_index2= cal.find(months_long[i])
         if cal_del_index != -1:
-            Calendar.month = i
+            Calendar = Calendar.replace(month=i)
             index=i
-#            if cal_del_index2 != -1:
+
 #                    cal = cal[cal_del_index2 : cal_del_index2 + len(months_long[i])]
 #            else:
 #                cal = cal[ cal_del_index1 : cal_del_index1 + len(months_short[i]) ]
@@ -44,9 +46,9 @@ def convertToCalendar(cal)
     #need to account for months in numeric form
     date_del_index= -1
     for i in range(1,dates[index]):
-        if cal.find(i) != -1:
-            Calendar.day = i
-            date_del_index = cal.find(i)
+        if cal.find(str(i)) != -1:
+            Calendar = Calendar.replace(day=i)
+            date_del_index = cal.find(str(i))
     
 #    if date_delete_index >= 0 and Calendar.day > 9:
 #        cal = cal [date_delete_index : date_delete_index + 2 ]
@@ -54,36 +56,35 @@ def convertToCalendar(cal)
 #        cal = cal[date_delete_index : date_delete_index + 1]
     year_del_index = -1
     for i in range (2000, today.year):
-        year_del_index = cal.find(i)
+        year_del_index = cal.find(str(i))
         if year_del_index != -1:
-            Calendar.year = i
+            Calendar = Calendar.replace(year=i)
     #note we need to do more to account for start and end date. We are using start date
     time_index = -1
     if cal.find("pm") != -1:
         time_index = find_n(cal, ' ', cal.find("pm"))
-        Calendar.hour = 12
+        Calendar = Calendar.replace(hour=12)
     if cal.find("p.m") != -1:
         time_index = find_n(cal, ' ', cal.find("p.m"))
-        Calendar.hour = 12
+        Calendar = Calendar.replace(hour=12)
     if cal.find("am") != -1:
         time_index = find_n(cal, ' ', cal.find("am"))
-        Calendar.hour = 0
+        Calendar = Calendar.replace(hour=0)
     if cal.find("a.m") != -1:
         time_index = find_n(cal, ' ', cal.find("a.m"))
-        Calendar.hour = 0;
+        Calendar = Calendar.replace(hour=0)
     if time_index != -1 and time_index >= len(cal) and isnumeric(cal[time_index]):
-        Calendar.hour += str(cal[time_index])
+        Calendar = Calendar.replace(hour = (Calendar.hour + int(cal[time_index])))
     #how do we delete time 
-    event_name = cal
     
-    if time_index > -1 and len(cal[:time_index]) < len(cal)
-        event_name=cal[:time_index]
-    if cal_del_index > -1 and len(cal[:cal_del_index]) < len(cal)
-        event_name=cal[:cal_del_index]
-    if year_del_index > -1 and len(cal[:year_del_index]) < len(cal)
-        event_name=cal[:year_del_index]
-    if date_del_index > -1 and len(cal[:date_del_index]) < len(cal)
-        event_name=cal[:date_del_index]
+    if time_index >= 0 and len(cal[:time_index]) < len(event_name):
+        event_name=cal[0:time_index]
+    if cal_del_index >= 0 and len(cal[:cal_del_index]) < len(event_name):
+        event_name=cal[0:cal_del_index]
+    if year_del_index >= 0 and len(cal[:year_del_index]) < len(event_name):
+        event_name=cal[0:year_del_index]
+    if date_del_index >= 0 and len(cal[:date_del_index]) < len(event_name):
+        event_name=cal[0:date_del_index]
 
 
 
@@ -104,5 +105,5 @@ def find_n(bigstring, searchterm, n):
     return image_to_string(img, config=custom_config)
 
 
-if __name__ == '__main__':
-    print(process_image("image2.jpg"))
+#if __name__ == '__main__':
+#    print(process_image("image2.jpg"))
