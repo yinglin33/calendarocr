54362432a24ca7711a005092e8391b11c0524559
bananahbatman <fasterthanlight1.1@gmail.com>
2021-01-04 22:08:55 +0000

    commented and renamed variables in convert_to_calendar

--- convert_to_calendar.py
+++ convert_to_calendar.py
@@ -1,107 +1,135 @@
 import datetime
 from PIL import Image
 import re
+'''
+Takes a string (cal) and looks for month, date, time, and event name in the string. Returns a string 
+containing the event name and Datetime object containing the month, date, time.
+'''
+
+''' 
+TODO change convertToCalendar so it returns the time interval of the event
+     todo account for leap years for feburary
+     might be a good idea to remove spaces from string to help with processing
+'''
 
 def convertToCalendar(cal):
-    months_short=["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
+    
+    #lists containing date keywords
+    monthShort=["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
     dates =[31,28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    months_long = ["january","february","march","april","may","june","july","august","september","october","november","december" ]
+    monthLong = ["january","february","march","april","may","june","july","august","september","october","november","december" ]
     days = [" mon", " tues", " wed", " thur", " fri"]
-    #todo account for leap years for feburary
+    
+    #list containing regex expressions for dates in the form of XX/XX/XXXX
+    possibleDateRegex = ["[0-1][0-9]/[0-3][0-9]/[0-9][0-9][0-9][0-9]", "[1-9]/[0-3][0-9]/[0-9][0-9][0-9][0-9]", "[0-1][0-9]/[1-9]/[0-9][0-9][0-9][0-9]", "[1-9]/[1-9]/[0-9][0-9][0-9][0-9]", "[0-1][0-9]/[0-3][0-9]/[0-9][0-9]", "[1-9]/[0-3][0-9]/[0-9][0-9]", "[0-1][0-9]/[1-9]/[0-9][0-9]", "[1-9]/[1-9]/[0-9][0-9]", "[0-1][0-9]/[0-3][0-9]", "[1-9]/[0-3][0-9]", "[0-1][0-9]/[1-9]", "[1-9]/[1-9]" ]
+ 
+    #Creates datetime object based on today's time
     today = datetime.datetime.today()
+    # made default datetime object using today's time
+    #the parameters are year, month, day, hour, minutes, seconds
     Calendar= datetime.datetime(today.year, today.month, today.day, 0, 0, 0)
-    # the parameters are year, month, day, hour, minutes, seconds
+    
+    #default event name   
+    eventName = cal
+
+    #tracks if the date is in regex form or XX/XX/XXXX 
+    dateNotRegex = True
+
     index= -1
-    #might be a good idea to remove spaces from string to help with processing
-    event_name = cal
-    possible_date_regex = ["[0-1][0-9]/[0-3][0-9]/[0-9][0-9][0-9][0-9]", "[1-9]/[0-3][0-9]/[0-9][0-9][0-9][0-9]", "[0-1][0-9]/[1-9]/[0-9][0-9][0-9][0-9]", "[1-9]/[1-9]/[0-9][0-9][0-9][0-9]", "[0-1][0-9]/[0-3][0-9]/[0-9][0-9]", "[1-9]/[0-3][0-9]/[0-9][0-9]", "[0-1][0-9]/[1-9]/[0-9][0-9]", "[1-9]/[1-9]/[0-9][0-9]", "[0-1][0-9]/[0-3][0-9]", "[1-9]/[0-3][0-9]", "[0-1][0-9]/[1-9]", "[1-9]/[1-9]" ]
-    date_not_regex = True
-    cal_del_index = -1
-    date_del_index= -1
-    for i in range( len(possible_date_regex)-1, -1, -1):
-        print(i)
-        x = re.findall(possible_date_regex[i], cal)
+
+    #Tracking these indexes to eventually determine the event name
+    calDelIndex = -1
+    dateDelIndex= -1
+    yearDelIndex = -1
+    timeIndex = -1 
+    day_index = -1
+    
+    #searches for matches from possibleDateRegex backwards. 
+    #This is because regex's at the end of the list are substrings of regex's at the beginning
+    for i in range( len(possibleDateRegex)-1, -1, -1):
+        x = re.findall(possibleDateRegex[i], cal)
         if len(x) != 0:
-            print("match")
-            cal_del_index = cal.find(x[0])
-            date_del_index = cal.find(x[0])
-            date_not_regex = False
+            calDelIndex = cal.find(x[0])
+            dateDelIndex = cal.find(x[0])
+            dateNotRegex = False
             info=[]
+
+            #splits the match into month, day, year
             info=x[0].split("/")
+            
+            #replaces month and day
             Calendar = Calendar.replace(month = int(info[0]))
             Calendar = Calendar.replace(day = int(info[1]))
+            
+            # replaces year based on if year was represented with 4 numbers or 2 numbers
+            # ex : 1/1/2021 vs 1/1/21
+            # if there was no year in the date year stays the same
             if len(info) == 3 and len(info[2]) == 4:
                 Calendar = Calendar.replace(year = int(info[2]))
             elif len(info) == 3 and len(info[2]) == 2:
                 Calendar = Calendar.replace( year = ((Calendar.year // 100)*100 + int(info[2])))
 
-    if date_not_regex:
-        for i in range(len(months_short)):
-            cal_del_index = cal.lower().find(months_short[i])
-            cal_del_index2= cal.lower().find(months_long[i])
-            if cal_del_index != -1:
+    # if date was found in regex no need to look for month or day
+    if dateNotRegex:
+
+        #searches for the month in the string using monthShort
+        for i in range(len(monthShort)):
+            
+            calDelIndex = cal.lower().find(monthShort[i])
+            calDelIndex2= cal.lower().find(monthLong[i])
+            if calDelIndex != -1:
                 Calendar = Calendar.replace(month=(i+1))
                 index=i
 
-#                    cal = cal[cal_del_index2 : cal_del_index2 + len(months_long[i])]
+#                    cal = cal[calDelIndex2 : calDelIndex2 + len(monthLong[i])]
 #            else:
-#                cal = cal[ cal_del_index1 : cal_del_index1 + len(months_short[i]) ]
+#                cal = cal[ calDelIndex1 : calDelIndex1 + len(monthShort[i]) ]
 
                 break
     #need to account for months in numeric form
         for i in range(1,dates[index]+1):
             if cal.find(" "+str(i)+" ") != -1 or cal.find(" " + str(i)) != -1 and (cal.find(" "+str(i)) + 1 + len(str(i))) < len(cal) or cal.find(str(i)+" ") != -1 and (cal.find(str(i)+" ") + 1 + len(str(i))) < len(cal) :
                 Calendar = Calendar.replace(day=i)
-                date_del_index = cal.find(str(i))
-#    if date_delete_index >= 0 and Calendar.day > 9:
-#        cal = cal [date_delete_index : date_delete_index + 2 ]
-#    elif date_delete_index >=0 :
-#        cal = cal[date_delete_index : date_delete_index + 1]
+                dateDelIndex = cal.find(str(i))
 
-    year_del_index = -1
     for i in range (2000, today.year+1):
-        year_del_index = cal.find(str(i))
-        if year_del_index != -1:
+        yearDelIndex = cal.find(str(i))
+        if yearDelIndex != -1:
             Calendar = Calendar.replace(year=i)
     #note we need to do more to account for start and end date. We are using start date
-    time_index = -1
     if cal.lower().find("pm") != -1:
-        time_index = find_n(cal, ' ', cal.lower().find("pm"))
+        timeIndex = find_n(cal, ' ', cal.lower().find("pm"))
         Calendar = Calendar.replace(hour=12)
     if cal.lower().find("p.m") != -1:
-        time_index = find_n(cal, ' ', cal.lower().find("p.m"))
+        timeIndex = find_n(cal, ' ', cal.lower().find("p.m"))
         Calendar = Calendar.replace(hour=12)
     if cal.lower().find("am ") != -1:
-        time_index = find_n(cal, ' ', cal.lower().find("am"))
+        timeIndex = find_n(cal, ' ', cal.lower().find("am"))
         Calendar = Calendar.replace(hour=0)
     if cal.lower().find("a.m") != -1:
-        time_index = find_n(cal, ' ', cal.lower().find("a.m"))
+        timeIndex = find_n(cal, ' ', cal.lower().find("a.m"))
         Calendar = Calendar.replace(hour=0)
-   # print(str(time_index))
-   # print(cal[time_index])
-    if time_index != -1 and time_index < len(cal) and cal[time_index].isnumeric():
-        #print("Time number:" + cal[time_index])
-        Calendar = Calendar.replace(hour = (Calendar.hour + int(cal[time_index])))
+    if timeIndex != -1 and timeIndex < len(cal) and cal[timeIndex].isnumeric():
+        Calendar = Calendar.replace(hour = (Calendar.hour + int(cal[timeIndex])))
 
-    #how do we delete time
-    day_index = -1
     for day in days:
         if cal.lower().find(day) != -1:
            day_index = cal.lower().find(day)
 
-    if time_index >= 0 and len(cal[:time_index]) < len(event_name):
-        event_name=cal[0:time_index]
-    if cal_del_index >= 0 and len(cal[:cal_del_index]) < len(event_name):
-        event_name=cal[0:cal_del_index]
-    if year_del_index >= 0 and len(cal[:year_del_index]) < len(event_name):
-        event_name=cal[0:year_del_index]
-    if date_del_index >= 0 and len(cal[:date_del_index]) < len(event_name):
-        event_name=cal[0:date_del_index]
-    if day_index >= 0 and len(cal[:day_index]) < len(event_name):
-        event_name = cal[0:day_index]
+    if timeIndex >= 0 and len(cal[:timeIndex]) < len(eventName):
+        eventName=cal[0:timeIndex]
+    if calDelIndex >= 0 and len(cal[:calDelIndex]) < len(eventName):
+        eventName=cal[0:calDelIndex]
+    if yearDelIndex >= 0 and len(cal[:yearDelIndex]) < len(eventName):
+        eventName=cal[0:yearDelIndex]
+    if dateDelIndex >= 0 and len(cal[:dateDelIndex]) < len(eventName):
+        eventName=cal[0:dateDelIndex]
+    if day_index >= 0 and len(cal[:day_index]) < len(eventName):
+        eventName = cal[0:day_index]
 
-    return Calendar, event_name
+    return Calendar, eventName
 
+# finds the instance of searchterm in bigstring that is before index n and the closest to index n
 def find_n(bigstring, searchterm, n):
     start = bigstring.find(searchterm)+1
     while start >= 0:
